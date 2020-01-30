2acd8cc3547573ef86cfdd74abaf27feeb56e2ac
roman <shmoop207@gmail.com>
2020-01-30 18:02:10 +0200

    export cache provider

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const decorators_1 = require("./module/src/decorators");
 exports.cache = decorators_1.cache;
+const cacheProvider_1 = require("./module/src/cacheProvider");
+exports.CacheProvider = cacheProvider_1.CacheProvider;
 const cacheModule_1 = require("./module/cacheModule");
 exports.CacheModule = cacheModule_1.CacheModule;
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
 "use strict";
 import {IOptions} from "./module/src/IOptions";
 import {cache} from "./module/src/decorators";
+import {CacheProvider} from "./module/src/cacheProvider";
 import {CacheModule} from "./module/cacheModule";
 
 export {IOptions}  from "./module/src/IOptions"
 
-export {CacheModule,cache}
+export {CacheModule,cache,CacheProvider}
